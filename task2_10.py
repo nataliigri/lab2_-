b7c09b7c46ecc7007cc04ec0f8e0416f49061bd4
nataliigri <103165792+nataliigri@users.noreply.github.com>
2023-11-03 08:38:31 +0200

    Update task2_10.py

--- task2_10.py
+++ task2_10.py
@@ -13,15 +13,15 @@ X4 = np.column_stack((mas_x**4, mas_x**3, mas_x**2, mas_x, np.ones_like(mas_x)))
 y4 = mas_y4_4_parabola + k1
 A4, B4, C4, D4, E4 = np.linalg.lstsq(X4, y4, rcond=None)[0]
 
-# Функція полінома четвертого ступеня
-def poly4_func(x):
+# Функція полінома п’ятого степеня
+def poly5_func(x):
     return A4 * x**4 + B4 * x**3 + C4 * x**2 + D4 * x + E4
 
 # Відображення точок та функції на графіку
 plt.scatter(mas_x, mas_y4_4_parabola, label='Задані точки')
 x_range = np.linspace(min(mas_x), max(mas_x), 100)
 
-plt.plot(x_range, poly4_func(x_range), color='red', label=f'y = {A4:.4f}x^4 + {B4:.4f}x^3 + {C4:.4f}x^2 + {D4:.4f}x + {E4:.4f}')
+plt.plot(x_range, poly5_func(x_range), color='red', label=f'y = {A4:.4f}x^4 + {B4:.4f}x^3 + {C4:.4f}x^2 + {D4:.4f}x + {E4:.4f}')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.legend()
